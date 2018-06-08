5a29d88fbb77944a6620a7995e866a72d7fb4d11
Piotr Stefanowicz <stefan8357@gmail.com>
2018-06-08 12:09:17 +0200

    Refactoring timelineDisplay function

--- scripts/timelineDisplay.js
+++ scripts/timelineDisplay.js
@@ -1,26 +1,22 @@
 const timelineDisplay = (dayIndex) => {
   const timelineContainer = $('.timeline');
   timelineContainer.empty();
-  console.log($('.timeline-mobile'))
   $('.timeline-mobile').empty();
   days[dayIndex].timeline.map((day) => {
-    timelineContainer.append($('<div class="card bg-light mb-3"></div>')
-      .append(`<div class="card-header">${day.time}</div>`)
-      .append($('<div class="card-body"></div>')
-        .append(`<h4 class="card-title">${day.temp}</h4>`)
-        .append(`<img class="card-icon" src=${defineIcon(day.weather)} alt="icon"/>`)
-        .append(`<p class="card-details">Ciśnienie:</p> <h6>${day.pressure} hPa</h6>`)
-        .append(`<p class="card-details">Wiatr:</p> <h6>${day.wind} km/h</h6>`)
-      )
-    );
-    ($('.timeline-mobile').eq(dayIndex).append($('<div class="card bg-light mb-3"></div>')
-      .append(`<div class="card-header">${day.time}</div>`)
-      .append($('<div class="card-body"></div>')
-        .append(`<h4 class="card-title">${day.temp}</h4>`)
-        .append(`<img class="card-icon" src=${defineIcon(day.weather)} alt="icon"/>`)
-        .append(`<p class="card-details">Ciśnienie:</p> <h6>${day.pressure} hPa</h6>`)
-        .append(`<p class="card-details">Wiatr:</p> <h6>${day.wind} km/h</h6>`)
-      ))
+
+    const appendTimeline = parent => {
+      parent.append($('<div class="card bg-light mb-3"></div>')
+        .append(`<div class="card-header">${day.time}</div>`)
+        .append($('<div class="card-body"></div>')
+          .append(`<h4 class="card-title">${day.temp}</h4>`)
+          .append(`<img class="card-icon" src=${defineIcon(day.weather)} alt="icon"/>`)
+          .append(`<p class="card-details">Ciśnienie:</p> <h6>${day.pressure} hPa</h6>`)
+          .append(`<p class="card-details">Wiatr:</p> <h6>${day.wind} km/h</h6>`)
+        )
+      );
+    };
+    appendTimeline(timelineContainer)
+    appendTimeline(($('.timeline-mobile').eq(dayIndex))
     );
   })
 };
