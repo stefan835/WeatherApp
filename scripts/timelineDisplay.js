7bccc76b290db84651cbbd28432194a667217b9c
Piotr Stefanowicz <stefan8357@gmail.com>
2018-06-08 11:57:47 +0200

    Adjusting media queries

--- scripts/timelineDisplay.js
+++ scripts/timelineDisplay.js
@@ -1,6 +1,8 @@
 const timelineDisplay = (dayIndex) => {
   const timelineContainer = $('.timeline');
   timelineContainer.empty();
+  console.log($('.timeline-mobile'))
+  $('.timeline-mobile').empty();
   days[dayIndex].timeline.map((day) => {
     timelineContainer.append($('<div class="card bg-light mb-3"></div>')
       .append(`<div class="card-header">${day.time}</div>`)
@@ -10,6 +12,15 @@ const timelineDisplay = (dayIndex) => {
         .append(`<p class="card-details">Ciśnienie:</p> <h6>${day.pressure} hPa</h6>`)
         .append(`<p class="card-details">Wiatr:</p> <h6>${day.wind} km/h</h6>`)
       )
-    )
+    );
+    ($('.timeline-mobile').eq(dayIndex).append($('<div class="card bg-light mb-3"></div>')
+      .append(`<div class="card-header">${day.time}</div>`)
+      .append($('<div class="card-body"></div>')
+        .append(`<h4 class="card-title">${day.temp}</h4>`)
+        .append(`<img class="card-icon" src=${defineIcon(day.weather)} alt="icon"/>`)
+        .append(`<p class="card-details">Ciśnienie:</p> <h6>${day.pressure} hPa</h6>`)
+        .append(`<p class="card-details">Wiatr:</p> <h6>${day.wind} km/h</h6>`)
+      ))
+    );
   })
 };
